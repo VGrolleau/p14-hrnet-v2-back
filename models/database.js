dc44e0cbe0be744da48e2885f76aff9266124391
VGrolleau <virginie.grolleau+github@protonmail.com>
2024-04-06 13:45:23 +0200

    Review and clean code

--- models/database.js
+++ models/database.js
@@ -2,12 +2,9 @@ const { Sequelize } = require('sequelize');
 
 // Init DB server
 const database = new Sequelize(
-    // 'hrnet', // DB name
-    // 'admin', // DB user
-    // 'Pa$$w0rd', // DB password
-    'lajo3289_hrnetvg', // DB name
-    'lajo3289_hrnetvg', // DB user
-    'RWSRke5ydxe}F_W', // DB password
+    'hrnet', // DB name
+    'admin', // DB user
+    'Pa$$w0rd', // DB password
     {
         host: 'localhost',
         dialect: 'mariadb'
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 // Enable CORS
 // app.use(cors());
 app.use((req, res, next) => {
+    console.log('Middleware CORS atteint');
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
@@ -16,7 +17,6 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use('/api', require('./routes/api'));
 
-// let server = app.listen(3001, () => {
-let server = app.listen(3306, () => {
+let server = app.listen(3001, () => {
     console.log(`Server started on port ${server.address().port}!`);
 });
